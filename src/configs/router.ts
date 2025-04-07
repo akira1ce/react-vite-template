339e19f9341b76c091ad88b119be9ff60b8d736b
akira1ce <akira1ce@163.com>
2025-04-07 17:25:36 +0800

    fix: add test routes

--- src/configs/router.ts
+++ src/configs/router.ts
@@ -3,6 +3,7 @@ import ErrorBoundary from '@/pages/ErrorBoundary';
 import Forbidden from '@/pages/Forbidden';
 import Home from '@/pages/home';
 import NotFound from '@/pages/NotFound';
+import Test from '@/pages/test/test';
 import { RouteObject } from 'react-router';
 
 export const routes: RouteObject[] = [
@@ -11,7 +12,9 @@ export const routes: RouteObject[] = [
     Component: BaseLayout,
     ErrorBoundary: ErrorBoundary,
     children: [
-      { index: true, Component: Home },
+      // { index: true, Component: Home },
+      { path: '/home', Component: Home },
+      { path: '/test', Component: Test },
       { path: '/403', Component: Forbidden },
       { path: '*', Component: NotFound },
     ],
