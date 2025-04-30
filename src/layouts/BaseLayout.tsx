8e2e9cfb3f096a30525cc791262cf836e5cbeb34
akira1ce <akira1ce@163.com>
2025-04-30 16:34:21 +0800

    feat: add GitHub link to BaseLayout for easy access to the repository

--- src/layouts/BaseLayout.tsx
+++ src/layouts/BaseLayout.tsx
@@ -1,9 +1,17 @@
+import { Github } from 'lucide-react';
 import React from 'react';
 import { Outlet } from 'react-router';
 
 const BaseLayout: React.FC = () => {
   return (
     <div className="relative flex h-screen w-screen items-center justify-center">
+      <a
+        className="absolute right-4 top-4 cursor-pointer rounded-md bg-gray-100 p-2 hover:bg-gray-200"
+        target="_blank"
+        href="https://github.com/akira1ce/react-vite-template"
+      >
+        <Github />
+      </a>
       <Outlet />
     </div>
   );
