f635768cab00bd9e681558258a3267163ca93430
akira1ce <akira1ce@163.com>
2025-07-10 17:33:37 +0800

    feat: conditionally render logout and GitHub link in BaseLayout based on authentication status

--- src/layouts/base-layout.tsx
+++ src/layouts/base-layout.tsx
@@ -4,20 +4,23 @@ import React from 'react';
 import { Outlet } from 'react-router';
 
 const BaseLayout: React.FC = () => {
-  const { logout } = useAuth();
+  const { logout, isAuth } = useAuth();
+  console.log('isAuth :>> ', isAuth);
 
   return (
     <div className="relative h-screen w-screen">
-      <div className="absolute right-4 top-4 flex items-center gap-2">
-        <LogOut className="cursor-pointer" onClick={logout} />
-        <a
-          className="cursor-pointer rounded-md bg-gray-100 p-2 hover:bg-gray-200"
-          target="_blank"
-          href="https://github.com/akira1ce/react-vite-template"
-        >
-          <Github />
-        </a>
-      </div>
+      {isAuth && (
+        <div className="absolute right-4 top-4 flex items-center gap-2">
+          <LogOut className="cursor-pointer" onClick={logout} />
+          <a
+            className="cursor-pointer rounded-md bg-gray-100 p-2 hover:bg-gray-200"
+            target="_blank"
+            href="https://github.com/akira1ce/react-vite-template"
+          >
+            <Github />
+          </a>
+        </div>
+      )}
       <Outlet />
     </div>
   );
