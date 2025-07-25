4cd90de1a45953c46654afce89be6f3f34b634a3
akira1ce <akira1ce@163.com>
2025-07-25 10:19:50 +0800

    chore: update yarn.lock with new dependencies and versions, and modify Test component layout for improved styling

--- src/pages/test/index.tsx
+++ src/pages/test/index.tsx
@@ -1,3 +1,7 @@
 export default function Test() {
-  return <div className="text-center font-bold">test</div>;
+	return (
+		<div className="flex h-full items-center justify-center text-center font-bold">
+			test
+		</div>
+	);
 }
