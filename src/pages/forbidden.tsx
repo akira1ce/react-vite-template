edbe8129e51fd3e832396822e9d76bf0dbc109da
akira1ce <akira1ce@163.com>
2025-07-25 13:42:49 +0800

    chore: rename forbidden name

---
+++ src/pages/forbidden.tsx
@@ -0,0 +1,16 @@
+import { Lock } from 'lucide-react';
+import { Link } from 'react-router';
+
+const Forbidden = () => {
+  return (
+    <div className="flex h-full w-full flex-col items-center justify-center gap-4">
+        <Lock className="mb-10 size-32 font-bold text-blue-500" />
+        <div className="text-center text-2xl text-gray-500">Forbidden</div>
+        <Link className="text-gray-400 underline" to={'/'}>
+          Back home
+        </Link>
+      </div>
+  );
+};
+
+export default Forbidden;
