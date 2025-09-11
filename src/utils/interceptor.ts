9d8909d2184ee66296bf4057881591918518cda9
akira1ce <akira1ce@163.com>
2025-09-11 13:33:37 +0800

    refactor(request): add qs middle & add download

--- src/utils/interceptor.ts
+++ src/utils/interceptor.ts
@@ -1,29 +1,31 @@
 import axios from "axios";
+import qs from "qs";
 import { getToken, removeToken } from "./auth";
 
 export const baseURL = import.meta.env.VITE_BASE_API_URL;
 
 const service = axios.create({
-  baseURL,
-  timeout: 10000,
-  withCredentials: false,
+	baseURL,
+	timeout: 10000,
+	withCredentials: false,
+	paramsSerializer: (params) => {
+		return qs.stringify(params, { arrayFormat: "repeat" });
+	},
 });
 
 service.interceptors.request.use((config) => {
-  const token = getToken();
-  if (token) config.headers.Authorization = `Bearer ${token}`;
-  return config;
+	const token = getToken();
+	if (token) config.headers.Authorization = `Bearer ${token}`;
+	return config;
 });
 
 service.interceptors.response.use((response) => {
-  const res = response.data;
+	if (response.data.code === 401) {
+		removeToken();
+		window.location.href = "/login";
+	}
 
-  if (res.code === 401) {
-    removeToken();
-    window.location.href = "/login";
-  }
-
-  return res;
+	return response;
 });
 
 export default service;
--- src/utils/request.ts
+++ src/utils/request.ts
@@ -1,46 +1,70 @@
-import type { AxiosRequestConfig } from "axios";
+import type { AxiosRequestConfig, AxiosResponse } from "axios";
+import { saveAs } from "file-saver";
 import type { Response } from "@/services/type";
 import instance from "./interceptor";
 
 export type ExtraOptions = Pick<AxiosRequestConfig, "timeout" | "headers" | "responseType">;
 
 function request<T>(url: string, params: any, method: string, options = {}) {
-  return new Promise<Response<T>>((resolve, reject) => {
-    let data = {};
+	return new Promise<T>((resolve, reject) => {
+		let data = {};
 
-    if (method === "get") data = { params };
-    else data = { data: params };
+		if (method === "get") data = { params };
+		else data = { data: params };
 
-    instance({
-      url,
-      method,
-      ...data,
-      ...options,
-    })
-      .then((res: any) => {
-        /* custom code to success or failed */
-        if (res.code === 0) resolve(res);
-        else reject(res);
-      })
-      .catch((err) => {
-        reject(err);
-        console.error(err);
-      });
-  });
+		instance({
+			url,
+			method,
+			...data,
+			...options,
+		})
+			.then((res) => {
+				/* 如果是blob，则直接返回 */
+				if (res.config?.responseType === "blob") {
+					resolve(res as any);
+				} else if (res.data.code === 0) {
+					resolve(res.data);
+				} else {
+					reject(res.data);
+				}
+			})
+			.catch((err) => {
+				reject(err);
+				console.error(err);
+			});
+	});
 }
 
 export function get<T, K = any>(url: string, params?: K, options?: ExtraOptions) {
-  return request<T>(url, params, "get", options);
+	return request<Response<T>>(url, params, "get", options);
 }
 
 export function post<T, K = any>(url: string, params?: K, options?: ExtraOptions) {
-  return request<T>(url, params, "post", options);
+	return request<Response<T>>(url, params, "post", options);
 }
 
 export function put<T, K = any>(url: string, params?: K, options?: ExtraOptions) {
-  return request<T>(url, params, "put", options);
+	return request<Response<T>>(url, params, "put", options);
 }
 
 export function del<T, K = any>(url: string, params?: K, options?: ExtraOptions) {
-  return request<T>(url, params, "delete", options);
+	return request<Response<T>>(url, params, "delete", options);
+}
+
+export async function download<T extends AxiosResponse, K = any>(url: string, params?: K, options?: ExtraOptions) {
+	const { filename = "download.xlsx", method = "get" } = (params as any) || {};
+	const res = await request<T>(url, params, method, { responseType: "blob", ...options });
+
+	// 处理文件名
+	let refinedFilename = filename;
+
+	const disposition = res.headers["content-disposition"];
+	if (disposition) {
+		const match = disposition.match(/filename="?([^"]+)"?/);
+		if (match && match[1]) {
+			refinedFilename = decodeURIComponent(match[1]);
+		}
+	}
+
+	saveAs(res.data, refinedFilename);
 }
