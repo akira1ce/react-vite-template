38501733043b2b27780e4667f4dbbaf75b38146c
akira1ce <akira1ce@163.com>
2026-03-19 14:27:19 +0800

    chore(dependencies): update package.json to add Tailwind CSS and r-hooks, remove postcss config, and adjust vite config for Tailwind integration

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,14 @@
+import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
-import autoprefixer from "autoprefixer";
 import { resolve } from "node:path";
-import tailwindcss from "tailwindcss";
 import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react()],
+	plugins: [react(), tailwindcss()],
 	resolve: {
 		alias: {
 			"@": resolve(__dirname, "src"),
-		},
-	},
-	css: {
-		postcss: {
-			plugins: [tailwindcss, autoprefixer],
 		},
 	},
 	server: {
