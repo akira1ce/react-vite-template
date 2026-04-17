491c05dc640a97a242c69d58fe3217d62e428ad8
akira1ce <akira1ce@163.com>
2026-04-17 16:57:44 +0800

    refactor(error pages): update styling and text for ErrorBoundary, Forbidden, and NotFound components to enhance readability and visual consistency

--- src/pages/error-boundary.tsx
+++ src/pages/error-boundary.tsx
@@ -1,13 +1,11 @@
-import { Ban } from "lucide-react";
 import type React from "react";
 import { Link } from "react-router";
 
 const ErrorBoundary: React.FC = () => {
 	return (
-		<div className="flex h-screen w-screen items-center justify-center">
+		<div className="flex h-screen w-screen flex-col items-center justify-center gap-4">
 			<div className="flex flex-col items-center justify-center gap-4">
-				<Ban className="mb-10 size-32 font-bold text-red-500" />
-				<div className="text-2xl">An unexpected error has occurred</div>
+				<div className="text-center text-4xl font-bold text-red-600">An unexpected error has occurred</div>
 				<Link className="text-gray-400 underline" to={"/"}>
 					Back home
 				</Link>
--- src/pages/forbidden.tsx
+++ src/pages/forbidden.tsx
@@ -1,11 +1,9 @@
-import { Lock } from "lucide-react";
 import { Link } from "react-router";
 
 const Forbidden = () => {
 	return (
 		<div className="flex h-screen w-screen flex-col items-center justify-center gap-4">
-			<Lock className="mb-10 size-32 font-bold text-blue-500" />
-			<div className="text-center text-2xl text-gray-500">Forbidden</div>
+			<div className="text-center text-4xl font-bold text-gray-500">Forbidden</div>
 			<Link className="text-gray-400 underline" to={"/"}>
 				Back home
 			</Link>
